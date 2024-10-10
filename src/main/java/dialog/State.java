d3e9fc8f41c930830f3da0c34853ea60c33e2eb8
bukinams <bukinams@yandex-team.ru>
2024-10-10 11:50:43 +0500

    wip shit commit

---
+++ src/main/java/dialog/State.java
@@ -0,0 +1,10 @@
+package dialog;
+
+import domain.command.Command;
+import domain.command.CommandType;
+
+import java.util.List;
+
+public class State {
+    public List<Command> commands;
+}
--- src/main/java/domain/command/Command.java
+++ src/main/java/domain/command/Command.java
@@ -1,4 +1,9 @@
 package domain.command;
 
+//public interface ICommand {
+//    stateFrom
+//    stateTo
+//}
+
 public record Command(CommandType type, String message) {
 }
