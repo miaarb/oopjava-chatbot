961be11b2152990d67e72f365b3239e4a685f1eb
bukinams <bukinams@yandex-team.ru>
2024-11-07 11:41:01 +0500

    add telegram bot handle command

--- src/main/java/app/telegramBot/TelegramBot.java
+++ src/main/java/app/telegramBot/TelegramBot.java
@@ -1,5 +1,9 @@
 package app.telegramBot;
 
+import dialog.UserDialog;
+import dialog.commands.*;
+import dialog.commands.abstractions.Command;
+import dialog.user.User;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
@@ -7,8 +11,19 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class TelegramBot implements LongPollingSingleThreadUpdateConsumer {
     private TelegramClient telegramClient = new OkHttpTelegramClient("");
+    private Map<Long, UserDialog> userDialogs = new HashMap<>();
+    private static final Map<String, Command> COMMANDS_MAP = Map.of(
+            "/add", new CreateCardCommand(),
+            "/help", new HelpCommand(),
+            "/read", new ReadCardCommand(),
+            "/show", new ShowAnswerCommand()
+    );
 
     @Override
     public void consume(Update update) {
@@ -17,11 +32,18 @@ public class TelegramBot implements LongPollingSingleThreadUpdateConsumer {
             // Set variables
             String message_text = update.getMessage().getText();
             long chat_id = update.getMessage().getChatId();
+            if (!userDialogs.containsKey(chat_id)) {
+                userDialogs.put(chat_id, new UserDialog(new User(UUID.randomUUID())));
+            }
+            var dialog = this.userDialogs.get(chat_id);
+
+            var command = COMMANDS_MAP.getOrDefault(message_text, new TextInputCommand(message_text));
+            var response = dialog.handleCommand(command);
 
             SendMessage message = SendMessage // Create a message object
                     .builder()
                     .chatId(chat_id)
-                    .text(message_text)
+                    .text(response.message())
                     .build();
             try {
                 telegramClient.execute(message); // Sending our message object to user
