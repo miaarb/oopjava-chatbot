3bb359f988055ff4bb9ffe0252a72d6a92985ee5
dengoan969 <dengoan969@gmail.com>
2024-11-25 11:20:45 +0500

    fix: remove unused handle input

--- src/main/java/dialog/state/ReadAnswerState.java
+++ src/main/java/dialog/state/ReadAnswerState.java
@@ -1,13 +1,12 @@
 package dialog.state;
 
-import dialog.internalCommands.handleTextInput.AnswerInputCommand;
 import dialog.user.User;
 
 public final class ReadAnswerState extends DialogState {
     public final String answer;
 
     public ReadAnswerState(User user, String answer) {
-        super(user, DialogStep.ANSWER_SHOW, new AnswerInputCommand());
+        super(user, DialogStep.ANSWER_SHOW, null);
         this.answer = answer;
     }
 }
